5ad96fe3771cbd9a10b91288043609b368ff158a
alievshamhal88-design <alievshamhal88@gmail.com>
2026-02-24 17:46:21 +0700

    Add configuration for admin IDs and post times

---
+++ config.py
@@ -0,0 +1,20 @@
+# ID администраторов
+ADMIN_IDS = [
+    7651760894,  # @cvetnik1_sib
+    7750251679,  # @Alan_Aliev
+]
+
+# Время публикации (если нужно для постера)
+POST_TIMES = [
+    "09:00",
+    "18:00"
+]
+
+# Состояния для новых модулей
+STATE_BIRTHDAY_WAITING = 100
+STATE_SUB_RECIPIENT = 101
+STATE_SUB_PHONE = 102
+STATE_SUB_ADDRESS = 103
+STATE_SUB_FREQUENCY = 104
+STATE_SUB_BUDGET = 105
+STATE_SUB_CONFIRM = 106
